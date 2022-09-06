9545b0215e45383e6761aabcb2830544df51b246
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-09-06 17:38:49 +0500

    Add files via upload

---
+++ axi.py
@@ -0,0 +1,9 @@
+import os
+import time
+os.system('git pull')
+
+print("")
+print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+
+from axi import ahmad
+ahmad() 
---
+++ uid.py
@@ -0,0 +1,9 @@
+import os
+import time
+os.system('git pull')
+
+print("")
+print("\033[1;32m     Welcome To My AXI Tool");time.sleep(2.0)
+
+from uid import Subscraption
+Subscraption() 
