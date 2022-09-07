6a038683060aa976c9aa0049241fd49aeaf29c9a
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-09-07 07:45:41 +0500

    Update file.py

--- file.py
+++ file.py
@@ -5,5 +5,32 @@ os.system('git pull')
 print("")
 print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
 
-from file import Subscraption
-Subscraption() 
+import os, platform, time
+
+os.system('git pull')
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+    
+    from file64 import Subscraption
+    
+    Subscraption() 
+
+elif b == '32bit':
+	
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+
+    from file import Subscraption
+    
+    Subscraption() 
