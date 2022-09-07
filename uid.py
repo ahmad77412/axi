633b1945d28a53595a230e2d868535d4309b79c9
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-09-07 07:47:23 +0500

    Update uid.py

--- uid.py
+++ uid.py
@@ -3,7 +3,34 @@ import time
 os.system('git pull')
 
 print("")
-print("\033[1;32m     Welcome To My AXI Tool");time.sleep(2.0)
+print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
 
-from uid import Subscraption
-Subscraption() 
+import os, platform, time
+
+os.system('git pull')
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+    
+    from uid64 import Subscraption
+    
+    Subscraption() 
+
+elif b == '32bit':
+	
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+
+    from uid import Subscraption
+    
+    Subscraption() 
