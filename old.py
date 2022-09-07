4de8cfb788f7136c9e3de95eeccd0e7bb2aa5553
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-09-07 06:54:02 +0500

    Update old.py

--- old.py
+++ old.py
@@ -3,7 +3,36 @@ import time
 os.system('git pull')
 
 print("")
-print("\033[1;32m     Welcome To My AXI Tool");time.sleep(2.0)
+print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
 
-from old import Subscraption
-Subscraption() 
+from axi import ahmad
+ahmad() 
+import os, platform, time
+
+os.system('git pull')
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+    
+    from old64 import Subscraption
+    
+    Subscraption() 
+
+elif b == '32bit':
+	
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+
+    from old import Subscraption
+    
+    Subscraption() 
