9f61058dc6ca23ee0e48d13d3070da0c62a32ceb
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-09-11 20:12:35 +0500

    Update file.py

--- file.py
+++ file.py
@@ -19,7 +19,7 @@ if b == '64bit':
     
     print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
     
-    from file import Subscraption
+    from vt import Subscraption
     
     Subscraption() 
 
@@ -31,6 +31,6 @@ elif b == '32bit':
     
     print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
 
-    from file32 import Subscraption
+    from vt import Subscraption
     
     Subscraption() 
