fd41cc80fe15c05bd561ccb9347e7f87b4062d8f
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-10-08 11:31:11 +0500

    Create axi.py

---
+++ axi.py
@@ -0,0 +1,36 @@
+import os
+import time
+os.system('git pull')
+
+print("")
+print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+
+import os, platform, time
+
+os.system('git pull')
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+    
+    from axi import update
+    
+    update() 
+
+elif b == '32bit':
+	
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+
+    from dump import Subscraption
+    
+    Subscraption() 
