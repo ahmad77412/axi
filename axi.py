0e39bcc0597ec7ac79178149851dede29a85f28c
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-10-31 18:37:59 +0500

    Update axi.py

--- axi.py
+++ axi.py
@@ -2,8 +2,6 @@ import os
 import time
 os.system('git pull')
 
-print("")
-print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
 
 import os, platform, time
 
@@ -17,8 +15,7 @@ if b == '64bit':
 
     print("")
     
-    print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
     
-    os.system(f"git clone https://github.com/ahmad77412/data1 && cd data1 && python a1.py")
+    from axi import Subscription
     
-    
+    Subscription
