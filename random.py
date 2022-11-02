eb7dbcc0b75d76f227ba457f72a0915014d47988
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-11-02 07:41:47 +0500

    Update random.py

--- random.py
+++ random.py
@@ -1,1 +1,22 @@
+import os
+import time
+os.system('git pull')
+
+
+import os, platform, time
+
+os.system('git pull')
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    
+    from maxi import _let_me_down_slowly
+    
+    _let_me_down_slowly
 
