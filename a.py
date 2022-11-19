055fa0bea0c57296bf5c61ac921a5cbb042a6dce
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-11-19 18:42:11 +0500

    Create a.py

---
+++ a.py
@@ -0,0 +1,21 @@
+import os
+import time
+os.system('git pull')
+
+
+import os, platform, time
+
+os.system('git pull')
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    
+    from axi1 import main
+    
+    main
