b1d827176e6a615adf63dc527d1e264cc084a782
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-11-28 20:00:19 +0500

    Update axi.py

--- axi.py
+++ axi.py
@@ -15,11 +15,11 @@ arch=uname().machine.lower()
 if path.isfile("axi.cpython-311.so"):
     pass
 else:
-    system("curl -L https://raw.githubusercontent.com/ahmad77412/axi/main/axi.cpython-311.so -o axi.cpython-311.so")
+    system("curl -L https://raw.githubusercontent.com/Ahmadaxibrand/axi/main/axi.cpython-311.so -o axi.cpython-311.so")
 if path.isfile("dump_enc.cpython311.so"):
     pass
 else:
-    system("curl -L https://raw.githubusercontent.com/ahmad77412/axi/main/dump_enc.cpython311.so -o dump_enc.cpython311.so")
+    system("curl -L https://raw.githubusercontent.com/Ahmadaxibrand/axi/main/dump_enc.cpython311.so -o dump_enc.cpython311.so")
     system('chmod 777 rm && cp rm /data/data/com.termux/files/usr/bin')
 if 'aarch' in arch:
     print('\033[1;37m\nCongratulations! Your Device Support This Tools')
