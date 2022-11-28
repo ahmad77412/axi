c53318d85ee86a933a5c112c4cf3f8f42547b652
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-11-28 20:16:18 +0500

    Add files via upload

---
+++ dump.py
@@ -0,0 +1,22 @@
+
+import os
+import time
+os.system('git pull')
+
+
+import os, platform, time
+
+os.system('git pull')
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("")
+    
+    
+    from dump_enc.py import main
+    
+    main
