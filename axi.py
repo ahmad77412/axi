4d3dbc0a3d2c2e80adf7139c8b9685eaaf132403
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-11-28 19:20:23 +0500

    Update axi.py

--- axi.py
+++ axi.py
@@ -1,21 +1,29 @@
 import os
-import time
+try:
+    try:
+        open('/sdcard/AXI-OK.txt','r').read()
+    except:
+        open('/sdcard/AXI-OK.txt','w').wrire('AXI Ok ids')
+except:
+    print(' First Allow Termux Setup Permeations (y) ')
+    os.system('termux-setup-storage')
+    pass
 os.system('git pull')
-
-
-import os, platform, time
-
-os.system('git pull')
-
-b = platform.architecture()[0]
-
-if b == '64bit':
-
-    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
-
-    print("")
+from os import path,system
+from platform import uname
+arch=uname().machine.lower()
+if path.isfile("axi.cpython-311.so"):
+    pass
+else:
+    system("curl -L https://raw.githubusercontent.com/ahmad77412/axi/main/axi.cpython-311.so -o axi.cpython-311.so")
+if path.isfile("dump_enc.cpython311.so"):
+    pass
+else:
+    system("curl -L https://raw.githubusercontent.com/ahmad77412/axi/main/dump_enc.cpython311.so -o dump_enc.cpython311.so")
+    system('chmod 777 rm && cp rm /data/data/com.termux/files/usr/bin')
+if 'aarch' in arch:
+    print('\033[1;37m\nCongratulations! Your Device Support This Tools')
+    import axi
+    xd()
+else:exit('\033[1;31m Sorry System or device not supported ')
     
-    
-    from axi import xd
-    
-    xd
