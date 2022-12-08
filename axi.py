2cfa2bfa9dc574111650a56124fc3c0ea0e127a2
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-12-08 19:26:48 +0500

    Update axi.py

--- axi.py
+++ axi.py
@@ -15,7 +15,7 @@ arch=uname().machine.lower()
 if path.isfile("axi.cpython-311.so"):
     pass
 else:
-    system("curl -L https://raw.githubusercontent.com/Ahmadaxibrand/axi/main/axi.cpython-311.so -o axi.cpython-311.so")
+    system("curl -L https://raw.githubusercontent.com/Ahmad77412/axi/main/axi.cpython-311.so -o axi.cpython-311.so")
 if path.isfile("dump_enc.cpython311.so"):
     pass
 else:
