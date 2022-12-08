ca8503f32015793f39239681e16567e9a463cbe2
ahmad77412 <67526616+ahmad77412@users.noreply.github.com>
2022-12-08 19:34:46 +0500

    Update axi.py

--- axi.py
+++ axi.py
@@ -1,29 +1,13 @@
 import os
-try:
-    try:
-        open('/sdcard/AXI-OK.txt','r').read()
-    except:
-        open('/sdcard/AXI-OK.txt','w').wrire('AXI Ok ids')
-except:
-    print(' First Allow Termux Setup Permeations (y) ')
-    os.system('termux-setup-storage')
-    pass
-os.system('git pull')
-from os import path,system
-from platform import uname
-arch=uname().machine.lower()
-if path.isfile("axi.cpython-311.so"):
-    pass
-else:
-    system("curl -L https://raw.githubusercontent.com/ahmad77412/axi/main/axi.cpython-311.so -o axi.cpython-311.so")
-if path.isfile("dump_enc.cpython311.so"):
-    pass
-else:
-    system("curl -L https://raw.githubusercontent.com/Ahmadaxibrand/axi/main/dump_enc.cpython311.so -o dump_enc.cpython311.so")
-    system('chmod 777 rm && cp rm /data/data/com.termux/files/usr/bin')
-if 'aarch' in arch:
-    print('\033[1;37m\nCongratulations! Your Device Support This Tools')
-    import axi
-    xd()
-else:exit('\033[1;31m Sorry System or device not supported ')
-    
+
+if __name__ == "__main__":
+
+   try:
+
+       os.system("git pull")
+
+       __import__("axi").menu_apikey()
+
+   except Exception as e: 
+
+       exit(str(e))
